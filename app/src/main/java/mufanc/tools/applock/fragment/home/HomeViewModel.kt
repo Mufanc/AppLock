c1b2b3e653ff318f451680da49641012bdc94456
Mufanc <1714511787@qq.com>
2022-04-05 00:30:30 +0800

    feat: show MIUI version in home page

--- app/src/main/java/mufanc/tools/applock/fragment/home/HomeViewModel.kt
+++ app/src/main/java/mufanc/tools/applock/fragment/home/HomeViewModel.kt
@@ -1,19 +1,26 @@
 package mufanc.tools.applock.fragment.home
 
+import android.os.SystemProperties
 import androidx.lifecycle.ViewModel
 import mufanc.tools.applock.BuildConfig
 import mufanc.tools.applock.MyApplication
 import mufanc.tools.applock.xposed.AppLockHelper
 
 class HomeViewModel : ViewModel() {
-    val isModuleActivated = MyApplication.isModuleActivated
-    val isServiceFound = MyApplication.processManager != null
-    val isHookerWorking = AppLockHelper.client != null
 
+    val isModuleActivated = MyApplication.isModuleActivated
     val versionName = BuildConfig.VERSION_NAME
-    val managerName = "${MyApplication.processManager}".removePrefix("android.os.")
+
+    val isServiceFound = MyApplication.processManager != null
+    val managerBinder = "${MyApplication.processManager}".removePrefix("android.os.")
+
+    val isHookerWorking = AppLockHelper.client != null
     val replyFromHook = AppLockHelper.client?.let {
         val reply = it.handshake()
         "pid:${reply[0]}, uid:${reply[1]}"
     } ?: "failed."
+
+    private val miuiVersion = SystemProperties.get("ro.miui.ui.version.code")
+    val isMiuiRom = miuiVersion.isNotEmpty()
+    val versionSummary = "Version: " + miuiVersion.ifBlank { "Unknown." }
 }

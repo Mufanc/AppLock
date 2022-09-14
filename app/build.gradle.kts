8cd1b349b7ee5693804748a65f469ac811f419fe
Mufanc <mufanc.xyz@gmail.com>
2022-09-14 21:50:46 +0800

    chore: update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -129,13 +129,13 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.13.2")
 
     implementation("com.google.android.material:material:1.6.1")
-    implementation("androidx.appcompat:appcompat:1.5.0")
+    implementation("androidx.appcompat:appcompat:1.5.1")
     implementation("androidx.core:core-ktx:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.5.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.5.1")
-    implementation("androidx.navigation:navigation-ui-ktx:2.5.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.2")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.2")
     implementation("androidx.preference:preference:1.2.0")
 
     implementation("com.google.code.gson:gson:2.9.1")

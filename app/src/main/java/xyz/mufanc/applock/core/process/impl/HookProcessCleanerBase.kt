73072127d0d8c0c44c9113a2533a61972fd0de63
Mufanc <mufanc.xyz@gmail.com>
2023-12-19 00:10:58 +0800

    feat: more process guard implements

--- app/src/main/java/xyz/mufanc/applock/core/process/impl/HookProcessCleanerBase.kt
+++ app/src/main/java/xyz/mufanc/applock/core/process/impl/HookProcessCleanerBase.kt
@@ -1,4 +1,22 @@
 package xyz.mufanc.applock.core.process.impl
 
-class HookProcessCleanerBase {
+import android.annotation.SuppressLint
+import io.github.libxposed.api.XposedInterface
+import xyz.mufanc.applock.core.process.model.ProcessInfo
+import xyz.mufanc.applock.core.process.model.ProcessInfoFactory
+import xyz.mufanc.applock.core.util.GraftClassLoader
+import java.lang.reflect.Method
+
+data object HookProcessCleanerBase : ProcessGuard.Adapter() {
+    @SuppressLint("PrivateApi")
+    override fun getMethod(): Method {
+        return GraftClassLoader.loadClass("com.android.server.am.ProcessCleanerBase")
+            .declaredMethods
+            .filter { it.name == "killOnce" && it.parameterTypes[0].simpleName == "ProcessRecord" }
+            .minByOrNull { it.parameterCount }!!
+    }
+
+    override fun getProcessInfo(callback: XposedInterface.BeforeHookCallback): ProcessInfo? {
+        return ProcessInfoFactory.create(callback.args[0]).takeIf { it.isValid }
+    }
 }

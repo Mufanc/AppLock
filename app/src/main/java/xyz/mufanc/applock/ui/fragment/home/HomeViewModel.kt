b51d780e2824d903652435ad5c1a3e484a9f546f
Mufanc <mufanc.xyz@gmail.com>
2024-02-07 00:44:23 +0800

    refactor: get framework info directly

--- app/src/main/java/xyz/mufanc/applock/ui/fragment/home/HomeViewModel.kt
+++ app/src/main/java/xyz/mufanc/applock/ui/fragment/home/HomeViewModel.kt
@@ -1,16 +1,16 @@
 package xyz.mufanc.applock.ui.fragment.home
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.map
+import xyz.mufanc.applock.App
 import xyz.mufanc.applock.BuildConfig
-import xyz.mufanc.applock.util.FrameworkInfo
 import xyz.mufanc.applock.util.I18n
 
 class HomeViewModel : ViewModel() {
 
-    val fwInfo = MutableLiveData<FrameworkInfo?>(null)
+    private val fwInfo = App.frameworkInfo.asLiveData()
 
     val isModuleLoaded = fwInfo.map { it != null }
 
